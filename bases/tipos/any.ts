85d8a001bdecf1c3404f85ca7dca87856292b7d0
ere <46026488+EreGitHub@users.noreply.github.com>
2022-06-07 23:00:57 -0400

    tipo any

---
+++ bases/tipos/any.ts
@@ -0,0 +1,44 @@
+(() => {
+	// // NOTE "ANY"
+	// let avenger: any = 123;
+	// // const exist; //NOTE error una constante tiene que esta inicializada
+	// let exist;
+	// let power;
+	// avenger = "Dr. Strange";
+	// console.log(avenger.charAt(0)); //NOTE no tengo el auto completado al ser any
+	// avenger = 150.1684;
+	// console.log(avenger.toFixed(2));
+	// console.log(exist);
+	// console.log(power);
+	// // NOTE "NUMBER"
+	// let avenger: number = 123;
+	// let exist;
+	// let power;
+	// avenger = "Dr. Strange"; //NOTE error no se puede asignar un string a un numero
+	// console.log(avenger.charAt(0)); //NOTE charAt() no funciona con un string
+	// avenger = 150.1684;
+	// console.log(avenger.toFixed(2));
+	// console.log(exist);
+	// console.log(power);
+	// // NOTE "STRING"
+	// let avenger: string = 123; //NOTE no se puede asignar un numeros a un string
+	// let exist;
+	// let power;
+	// avenger = "Dr. Strange";
+	// console.log(avenger.charAt(0));
+	// avenger = 150.1684; //NOTE error no se puede asignar un numero a un string
+	// console.log(avenger.toFixed(2)); //NOTE toFixed() no funciona con un string
+	// console.log(exist);
+	// console.log(power);
+
+	// NOTE Casting "as"
+	let avenger: any = 123;
+	let exist;
+	let power;
+	avenger = "Dr. Strange";
+	console.log((avenger as string).charAt(0));
+	avenger = 150.1684;
+	console.log((<number>avenger).toFixed(2));
+	console.log(exist);
+	console.log(power);
+})();
