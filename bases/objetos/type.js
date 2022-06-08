2761967d7b6454c4329c7583997a4e81c63637ea
ere <46026488+EreGitHub@users.noreply.github.com>
2022-06-08 15:34:18 -0400

    Tipos personalizados

---
+++ bases/objetos/type.js
@@ -0,0 +1,17 @@
+"use strict";
+(function () {
+    var flash = {
+        name: "ruben",
+        age: 24,
+        powers: ["speed", "fly", "xray"],
+    };
+    var superman = {
+        name: "ruben2",
+        age: 20,
+        powers: ["speed", "xray"],
+        getName: function () {
+            return this.name;
+        },
+    };
+    console.log(flash);
+})();
