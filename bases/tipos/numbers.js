5c52030fdc6c29f371572410661a1a6028f7d4a9
ere <46026488+EreGitHub@users.noreply.github.com>
2022-06-07 22:20:47 -0400

    numbers (op1)

---
+++ bases/tipos/numbers.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    let avengers;
+    const villanos = 20;
+    if (avengers < villanos) {
+        console.log("Estamos en problemas");
+    }
+    else {
+        console.log("nos salvamos");
+    }
+})();
---
+++ bases/tipos/numbers.ts
@@ -0,0 +1,10 @@
+(() => {
+	let avengers;
+	const villanos: number = 20;
+
+	if (avengers < villanos) {
+		console.log("Estamos en problemas");
+	} else {
+		console.log("nos salvamos");
+	}
+})();
