a7c3ab5cb274b3fff2f9aba0a6b7c9da20c3fefd
ere <46026488+EreGitHub@users.noreply.github.com>
2022-06-07 23:11:54 -0400

    array

---
+++ bases/tipos/arrays.js
@@ -0,0 +1,10 @@
+"use strict";
+(function () {
+    // const numbers: (string | number)[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, "10"];
+    // // numbers.push(true); //NOTE solo numeros o strings
+    // numbers.push(11);
+    // console.log(numbers);
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var villanos = ["Omega Rojo", "Dormammu", "Duende Verde"];
+    villanos.forEach(function (v) { return console.log(v.toUpperCase()); });
+})();
---
+++ bases/tipos/arrays.ts
@@ -0,0 +1,10 @@
+(() => {
+	// const numbers: (string | number)[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, "10"];
+	// // numbers.push(true); //NOTE solo numeros o strings
+	// numbers.push(11);
+	// console.log(numbers);
+
+	const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	const villanos: string[] = ["Omega Rojo", "Dormammu", "Duende Verde"];
+	villanos.forEach((v) => console.log(v.toUpperCase()));
+})();
