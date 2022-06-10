0f07308612569f02fb5e5dd65c1175ff574cc07f
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-10 19:51:47 -0400

    Decoradores de fabrica-Factory decorator

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -2,7 +2,13 @@ function printToConsole(constructor: Function) {
 	console.log(constructor);
 }
 
-@printToConsole
+const printToConsoleConditional = (print: boolean = false): Function => {
+	// return () => console.log("Hello World");
+	if (print) return printToConsole; //NOTE aqui le pasamos una referencia
+	else return () => {}; //NOTE aqui le pasamos una funcion vacia
+};
+
+@printToConsoleConditional(true)
 export class Pokemon {
 	public api: string = "https://pokemonapi.co";
 	constructor(public name: string) {}
