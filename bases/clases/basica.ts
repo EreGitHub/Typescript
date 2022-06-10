0f5042d8fd7a25e0601136c7c2de3140cdb78807
dev <36090969+dcorvera@users.noreply.github.com>
2022-06-10 11:55:10 -0400

    forma corta de asignar propiedades

--- bases/clases/basica.ts
+++ bases/clases/basica.ts
@@ -1,15 +1,11 @@
 (() => {
 	class Avenger {
-		private name: string;
-		public team: string;
-		public realName?: string;
-		static avgage: number = 35;
-
-		constructor(name: string, team: string, realName?: string) {
-			this.name = name;
-			this.team = team;
-			this.realName = realName;
-		}
+		static avgage: number = 40;
+		constructor(
+			private name: string,
+			public team: string,
+			public realName?: string
+		) {}
 	}
 
 	const antman: Avenger = new Avenger("Antman", "Capitan");
