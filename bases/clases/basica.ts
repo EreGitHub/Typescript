20af13c4d583ae0ad76c75f9e2999351673a4c92
dev <36090969+dcorvera@users.noreply.github.com>
2022-06-10 12:06:27 -0400

    Metodos publicos y privados

--- bases/clases/basica.ts
+++ bases/clases/basica.ts
@@ -1,14 +1,27 @@
 (() => {
 	class Avenger {
 		static avgage: number = 40;
+		static getAvgAge() {
+			return this.name;
+		}
+
 		constructor(
 			private name: string,
-			public team: string,
+			private team: string,
 			public realName?: string
 		) {}
+
+		// private bio(): string {
+		public bio(): string {
+			return `${this.name} (${this.team})!!!`;
+		}
 	}
 
 	const antman: Avenger = new Avenger("Antman", "Capitan");
 	console.log(antman);
 	console.log(Avenger.avgage);
+
+	console.log(antman.bio());
+
+	console.log(Avenger.getAvgAge());
 })();
