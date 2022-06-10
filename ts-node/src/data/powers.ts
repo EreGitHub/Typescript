74c055b1407c9bb971ad7b540eda1e23a44c8922
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-10 17:17:05 -0400

    Export Default y esportacion con alias

---
+++ ts-node/src/data/powers.ts
@@ -0,0 +1,17 @@
+export interface Powers {
+	id: number;
+	desc: string;
+}
+
+const powers: Powers[] = [
+	{
+		id: 1,
+		desc: "Super speed",
+	},
+	{
+		id: 2,
+		desc: "Super strength",
+	},
+];
+
+export default powers;
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,4 +1,13 @@
-import { Hero } from "./clases/Hero";
+// import { Hero as SuperHero, Hero2, Hero3, Hero4 } from "./clases/Hero";
+// const Hero = 123;
+// const iroman = new SuperHero("Iroman", 100, 30);
+// console.log(iroman);
 
-const iroman = new Hero("Iroman", 100, 30);
+import * as Clases from "./clases/Hero";
+import powersDefaul, { Powers } from "./data/powers";
+
+const Hero = 123;
+const iroman = new Clases.Hero("Iroman", 100, 30);
 console.log(iroman);
+
+console.log(powersDefaul);
