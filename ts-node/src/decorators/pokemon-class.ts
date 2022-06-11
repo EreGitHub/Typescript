040a3b790f44cddc102589e8fddbf6eb4da70028
dev <46026488+EreGitHub@users.noreply.github.com>
2022-06-10 20:47:13 -0400

    decoradores de propiedad

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -1,28 +1,26 @@
-function CheckValidPokemonId() {
-	return function (
-		target: any,
-		propertyKey: string,
-		descriptor: PropertyDescriptor
-	) {
-		// console.log({ target, propertyKey, descriptor });
-		// descriptor.value = () => {
-		// 	console.log("hola mundo");
-		// };
-		const originalMethod = descriptor.value;
-		descriptor.value = (id: number) => {
-			if (id < 1 || id > 800) return console.error("id invalido");
-			else return originalMethod(id);
+function readonly(isWritable: boolean = true): Function {
+	return function (target: any, propertyKey: string) {
+		const descriptor: PropertyDescriptor = {
+			get() {
+				console.log(this);
+				return "Pepe";
+			},
+			set(this, val) {
+				Object.defineProperty(this, propertyKey, {
+					value: val,
+					writable: !isWritable,
+					enumerable: false,
+				});
+			},
 		};
+		return descriptor;
 	};
 }
 
-//NOTE ejecuta en orden secuencial
 export class Pokemon {
+	// @readonly(false)
+	@readonly()
 	public api: string = "https://pokemonapi.co";
-	constructor(public name: string) {}
 
-	@CheckValidPokemonId()
-	savePokemonToDB(id: number) {
-		console.log(`pkemon guardado ${id}`);
-	}
+	constructor(public name: string) {}
 }
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -2,6 +2,5 @@ import { Pokemon } from "./decorators/pokemon-class";
 
 const charmander = new Pokemon("Charmander");
 
-// (Pokemon.prototype as any).customName = "pikachu";
-
-charmander.savePokemonToDB(1000);
+charmander.api = "https://youtube.com";
+console.log(charmander);
